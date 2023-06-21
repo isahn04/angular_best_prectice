526e3d42ac7df0f262c520c60b43633fa4ba79b6
Nikunj1896 <nikunj.projects9893@gmail.com>
2023-06-21 17:19:28 +0530

    third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/user/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from '@core/auth/login/login.component';
 import { SignupComponent } from '@core/auth/signup/signup.component';
 
