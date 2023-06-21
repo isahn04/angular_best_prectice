e9968a7a8356e0dcb9689d64a604132a1da9bf9a
Nikunj1896 <nikunj.projects9893@gmail.com>
2023-06-21 17:06:28 +0530

    create user compone tand login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { HomeComponent } from './pages/user/home/home.component';
+import { LoginComponent } from '@core/auth/login/login.component';
+import { SignupComponent } from '@core/auth/signup/signup.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
-import { HomeModule } from './pages/home/home.module';
+import { HomeModule } from './pages/user/home/home.module';
 
 @NgModule({
   declarations: [
